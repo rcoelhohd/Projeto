3127785efb4ee1c12a683325bccfdbadd6fae941
rcoelhohd <rcoelho.santos@hotmail.com>
2023-03-10 12:47:09 -0300

    Teste de envio, antes da codificacao

---
+++ Calculadora/src/main/java/com/rafaelcoelho/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rafaelcoelho.calculadora;
+
+/**
+ *
+ * @author rcoel
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
